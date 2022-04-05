1e04acfebc8d5e758cfc7f93edcb8aeec99bdd0d
thanhtrung0301 <56642658+thanhtrung0301@users.noreply.github.com>
2022-04-05 17:28:13 +0700

    Delete public/javascripts directory

--- public/javascripts/product-pagination.js
+++
@@ -1,4 +0,0 @@
-const pageList = document.querySelector('.product-pagination');
-const pageCurrent = pageList.getAttribute('pageCurrent');
-
-(pageList.children)[pageCurrent].classList.add('current');
