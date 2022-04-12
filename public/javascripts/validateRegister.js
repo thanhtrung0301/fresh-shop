ddbdc8d18a452494adc27e86f3800a7475757740
thanhtrung0301 <56642658+thanhtrung0301@users.noreply.github.com>
2022-04-12 21:54:56 +0700

    Add files via upload

--- public/javascripts/validateRegister.js
+++ public/javascripts/validateRegister.js
@@ -4,46 +4,18 @@ function Validator() {
     const password = document.getElementById('password');
     const c_password = document.getElementById('c_password');
 
-    let flag = 0;
-    if(username.value.length === 0) {
-        let usernameError = username.parentElement.querySelector('span');
-        usernameError.innerText = 'You must enter username';
-        usernameError.style.color = 'red';
-        flag--;
-    }
-    else {
-        flag++;
-        let usernameError = username.parentElement.querySelector('span');
-        usernameError.innerText = '';
-    }
+    let check = true;
+    if(username.value.length === 0) check = false;
 
+    const isNotExist = username.getAttribute('check')
+    if(!isNotExist) check = false;
 
-    if((/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/).test(password.value) === false) {
-        let passError = password.parentElement.querySelector('span');
-        passError.innerText = 'You password is too weak ';
-        passError.style.color = 'red';
-        flag--;
-    }
-    else {
-        flag++;
-        let passError = password.parentElement.querySelector('span');
-        passError.innerText = '';
-    }
-
-    if(password.value !== c_password.value) {
-        let confirmError = c_password.parentElement.querySelector('span');
-        confirmError.innerText = 'Password and confirm password not match';
-        confirmError.style.color = 'red';
-        flag--;
-    }
-    else {
-        flag++;
-        let confirmError = c_password.parentElement.querySelector('span');
-        confirmError.innerText = '';
-    }
-
-    console.log(flag);
-    if (flag === 3) {
+    if(!(/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/).test(password.value)) check = false;
+    
+    if(password.value !== c_password.value || password.value === '') check = false;
+      
+    console.log(check)
+    if (check) {
         document.getElementById('registerForm').submit();
         return true;  
     }
